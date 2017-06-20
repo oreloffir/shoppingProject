57c5e457ae4710f3b8b7decacac0c802966c4c4d
Orel <oreloffir@gmail.com>
2017-06-20 20:21:47 +0300

    Login Logout

--- views/fragments/header.php
+++ views/fragments/header.php
@@ -39,6 +39,8 @@
                         <!-- Header -->
                         <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            <h3 class="display-4">Login:</h3>
+
                         </div>
 
                         <!-- Body -->
